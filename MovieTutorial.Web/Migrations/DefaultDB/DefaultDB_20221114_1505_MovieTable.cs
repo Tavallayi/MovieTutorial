1697f23547373dfbaa27b185abe01002cf31dafd
tavallayi <tavallayi@vision-it.ir>
2023-12-04 14:14:26 +0330

    Init Movie table and classes.

---
+++ MovieTutorial.Web/Migrations/DefaultDB/DefaultDB_20221114_1505_MovieTable.cs
@@ -0,0 +1,23 @@
+﻿using FluentMigrator;
+
+namespace MovieTutorial.Migrations.DefaultDB;
+
+public partial class DefaultDB_20161029_1300_ExceptionLog
+{
+    [DefaultDB, MigrationKey(20221114_1505)]
+    public class DefaultDB_20221114_1505_MovieTable : AutoReversingMigration
+    {
+        public override void Up()
+        {
+            Create.Table("Movie")
+                .WithColumn("MovieId").AsInt32()
+                    .Identity().PrimaryKey().NotNullable()
+                .WithColumn("Title").AsString(200).NotNullable()
+                .WithColumn("Description").AsString(1000).Nullable()
+                .WithColumn("Storyline").AsString(Int32.MaxValue).Nullable()
+                .WithColumn("Year").AsInt32().Nullable()
+                .WithColumn("ReleaseDate").AsDateTime().Nullable()
+                .WithColumn("Runtime").AsInt32().Nullable();
+        }
+    }
+}
