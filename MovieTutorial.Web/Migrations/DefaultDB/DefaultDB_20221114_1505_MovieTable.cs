390ba041b710d284dbfaf334e233a2b37d8315a8
tavallayi <tavallayi@vision-it.ir>
2023-12-04 14:45:02 +0330

    Customizing Quick Search

--- MovieTutorial.Web/Migrations/DefaultDB/DefaultDB_20221114_1505_MovieTable.cs
+++ MovieTutorial.Web/Migrations/DefaultDB/DefaultDB_20221114_1505_MovieTable.cs
@@ -20,27 +20,4 @@ public partial class DefaultDB_20161029_1300_ExceptionLog
                 .WithColumn("Runtime").AsInt32().Nullable();
         }
     }
-    [DefaultDB, MigrationKey(20221114_1703)]
-    public class DefaultDB_20221114_1703_MovieData : AutoReversingMigration
-    {
-        public override void Up()
-        {
-            Insert.IntoTable("Movie")
-                .Row(new
-                {
-                    Title = "The Matrix",
-                    Year = 1999,
-                })
-                .Row(new
-                {
-                    Title = "Pulp fiction",
-                    Year = 1994,
-                })
-                .Row(new
-                {
-                    Title = "The Lord of the Reings",
-                    Year = 1999,
-                });
-        }
-    }
 }
---
+++ MovieTutorial.Web/Migrations/DefaultDB/DefaultDB_20221114_1703_MovieData.cs
@@ -0,0 +1,30 @@
+﻿using FluentMigrator;
+
+namespace MovieTutorial.Migrations.DefaultDB;
+
+public partial class DefaultDB_20161029_1300_ExceptionLog
+{
+    [DefaultDB, MigrationKey(20221114_1703)]
+    public class DefaultDB_20221114_1703_MovieData : AutoReversingMigration
+    {
+        public override void Up()
+        {
+            Insert.IntoTable("Movie")
+                .Row(new
+                {
+                    Title = "The Matrix",
+                    Year = 1999,
+                })
+                .Row(new
+                {
+                    Title = "Pulp fiction",
+                    Year = 1994,
+                })
+                .Row(new
+                {
+                    Title = "The Lord of the Reings",
+                    Year = 1999,
+                });
+        }
+    }
+}
